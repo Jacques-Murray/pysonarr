8449988c2b674b331cf4ff7b5ae2c6c337f76a7c
Jacques Murray <jacquesmmurray@gmail.com>
2024-08-05 09:38:43 +0200

    feat: Add pysonarr.enums module with various enum classes

---
+++ pysonarr/enums/__init__.py
@@ -0,0 +1,19 @@
+"""
+pysonarr.enums
+"""
+
+from .apply_tags import ApplyTags
+from .authentication_required_type import AuthenticationRequiredType
+from .authentication_type import AuthenticationType
+from .backup_type import BackupType
+from .monitor_types import MonitorTypes
+from .privacy_level import PrivacyLevel
+
+__all__ = [
+    "ApplyTags",
+    "AuthenticationRequiredType",
+    "AuthenticationType",
+    "BackupType",
+    "MonitorTypes",
+    "PrivacyLevel",
+]
