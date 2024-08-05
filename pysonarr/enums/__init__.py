9d945c61f10b803d28de355cb1f81462c5ebcddb
Jacques Murray <jacquesmmurray@gmail.com>
2024-08-05 10:35:40 +0200

    feat: Add new Sonarr schema classes and enums

--- pysonarr/enums/__init__.py
+++ pysonarr/enums/__init__.py
@@ -6,14 +6,26 @@ from .apply_tags import ApplyTags
 from .authentication_required_type import AuthenticationRequiredType
 from .authentication_type import AuthenticationType
 from .backup_type import BackupType
+from .download_protocol import DownloadProtocol
+from .media_cover_types import MediaCoverTypes
 from .monitor_types import MonitorTypes
+from .new_item_monitor_types import NewItemMonitorTypes
 from .privacy_level import PrivacyLevel
+from .quality_source import QualitySource
+from .series_status_type import SeriesStatusType
+from .series_types import SeriesTypes
 
 __all__ = [
     "ApplyTags",
     "AuthenticationRequiredType",
     "AuthenticationType",
     "BackupType",
+    "DownloadProtocol",
+    "MediaCoverTypes",
     "MonitorTypes",
+    "NewItemMonitorTypes",
     "PrivacyLevel",
+    "QualitySource",
+    "SeriesStatusType",
+    "SeriesTypes",
 ]
--- pysonarr/schemas/__init__.py
+++ pysonarr/schemas/__init__.py
@@ -8,8 +8,21 @@ from .auto_tagging_resource import AutoTaggingResource
 from .auto_tagging_specification_schema import AutoTaggingSpecificationSchema
 from .backup_resource import BackupResource
 from .blocklist_bulk_resource import BlocklistBulkResource
+from .blocklist_resource import BlocklistResource
+from .custom_format_resource import CustomFormatResource
+from .custom_format_specification_schema import CustomFormatSpecificationSchema
 from .field import Field
+from .language import Language
+from .media_cover import MediaCover
+from .quality import Quality
+from .quality_model import QualityModel
+from .ratings import Ratings
+from .revision import Revision
+from .season_resource import SeasonResource
+from .season_statistics_resource import SeasonStatisticsResource
 from .select_option import SelectOption
+from .series_resource import SeriesResource
+from .series_statistics_resource import SeriesStatisticsResource
 
 __all__ = [
     "AddSeriesOptions",
@@ -18,6 +31,19 @@ __all__ = [
     "AutoTaggingSpecificationSchema",
     "BackupResource",
     "BlocklistBulkResource",
+    "BlocklistResource",
+    "CustomFormatResource",
+    "CustomFormatSpecificationSchema",
     "Field",
+    "Language",
+    "MediaCover",
+    "Quality",
+    "QualityModel",
+    "Ratings",
+    "Revision",
+    "SeasonResource",
+    "SeasonStatisticsResource",
     "SelectOption",
+    "SeriesResource",
+    "SeriesStatisticsResource",
 ]
